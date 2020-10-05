8e63a4b42d2276e32cec8b6fd887c6b58de0156d
Shun-Prog <shun.prog.tanaka@gmail.com>
2020-10-05 11:06:38 +0900

    タスク名のmodel spec追加

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+
+  describe 'タスク名のバリデーション' do
+
+    context 'タスク名が無い場合' do
+      task = Task.new(description: 'description')
+  
+      it '無効である' do
+        expect(task).to be_invalid
+      end
+  
+      it "can't be blankとエラーメッセージが出る" do
+        task.valid?
+        expect(task.errors[:name]).to include("can't be blank")
+      end
+    end
+  
+    context 'タスク名が31文字以上の場合' do
+      task = Task.new(name: 'a' * 31, description: 'description')
+  
+      it '無効である' do
+        expect(task).to be_invalid
+      end
+  
+      it "is too long (maximum is 30 characters)とエラーメッセージが出る" do
+        task.valid?
+        expect(task.errors[:name]).to include("is too long (maximum is 30 characters)")
+      end
+  
+    end
+  
+    context 'タスク名が30文字以内の場合' do
+      task = Task.new(name: 'a' * 30, description: 'description')
+  
+      it '有効である' do
+        expect(task).to be_valid
+      end
+  
+    end
+  end
+  
+  # it 'タスク詳細は1001文字以上である場合、無効である'
+  # it 'タスク詳細は1000文字以内である場合、有効である'
+  # it 'タスク詳細が無い場合、無効である'
+end
