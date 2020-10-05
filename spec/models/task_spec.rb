7a1a0d8b90b539013f3b6a6b4d607845f7efd255
Shun-Prog <shun.prog.tanaka@gmail.com>
2020-10-05 11:38:01 +0900

    タスク詳細のmode spec追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -10,11 +10,12 @@ RSpec.describe Task, type: :model do
       it '無効である' do
         expect(task).to be_invalid
       end
-  
+
       it "can't be blankとエラーメッセージが出る" do
         task.valid?
         expect(task.errors[:name]).to include("can't be blank")
       end
+
     end
   
     context 'タスク名が31文字以上の場合' do
@@ -24,7 +25,7 @@ RSpec.describe Task, type: :model do
         expect(task).to be_invalid
       end
   
-      it "is too long (maximum is 30 characters)とエラーメッセージが出る" do
+      it "is too long~ とエラーメッセージが出る" do
         task.valid?
         expect(task.errors[:name]).to include("is too long (maximum is 30 characters)")
       end
@@ -40,8 +41,44 @@ RSpec.describe Task, type: :model do
   
     end
   end
+
+
+  describe 'タスク詳細のバリデーション' do
+
+    context 'タスク詳細が無い場合' do
+      task = Task.new(name: 'name')
   
-  # it 'タスク詳細は1001文字以上である場合、無効である'
-  # it 'タスク詳細は1000文字以内である場合、有効である'
-  # it 'タスク詳細が無い場合、無効である'
+      it '無効である' do
+        expect(task).to be_invalid
+      end
+  
+      it "can't be blankとエラーメッセージが出る" do
+        task.valid?
+        expect(task.errors[:description]).to include("can't be blank")
+      end
+
+    end
+
+    context 'タスク詳細が1001文字以上の場合' do
+      task = Task.new(name: 'name', description: 'a' * 1001)
+  
+      it '無効である' do
+        expect(task).to be_invalid
+      end
+  
+      it "is too long~ とエラーメッセージが出る" do
+        task.valid?
+        expect(task.errors[:description]).to include("is too long (maximum is 1000 characters)")
+      end
+    end
+
+    context 'タスク詳細が1000文字以内の場合' do
+      task = Task.new(name: 'name', description: 'a' * 1000)
+  
+      it '有効である' do
+        expect(task).to be_valid
+      end
+  
+    end
+  end
 end
