2f6bc2e58e80cb616454dd73549593b819ccee64
Shun-Prog <shun.prog.tanaka@gmail.com>
2020-10-06 22:17:32 +0900

    テスト考慮漏れ修正

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -19,10 +19,11 @@ RSpec.describe 'Tasks', type: :system do
     it '一覧で表示される' do
       expect(page).to have_content task.name
       expect(page).to have_content task.created_at
+      expect(page).to have_content task.expired_at
     end
 
     it '作成日の降順で並ぶ' do
-      expect(rows[0].find('.task_created_at').text).to eq task_add_1year.created_at.to_s 
+      expect(rows[0].find('.task_created_at').text).to eq task_add_1year.created_at.to_s # 現在日時 + 1年
       expect(rows[1].find('.task_created_at').text).to eq task_add_1day.created_at.to_s # 現在日時 + 1日
       expect(rows[2].find('.task_created_at').text).to eq task_add_1hour.created_at.to_s # 現在日時 + 1時間
       expect(rows[3].find('.task_created_at').text).to eq task.created_at.to_s # 現在日時
@@ -56,6 +57,7 @@ RSpec.describe 'Tasks', type: :system do
       visit task_path(task)
       expect(page).to have_content task.name
       expect(page).to have_content task.description
+      expect(page).to have_content task.expired_at
       expect(page).to have_content task.created_at
     end
 
@@ -67,17 +69,20 @@ RSpec.describe 'Tasks', type: :system do
       visit new_task_path
       fill_in 'タスク名',	with: name
       fill_in 'タスク詳細',	with: description
+      fill_in '終了期限',	with: expired_at
       click_button '保存'
     end
 
     context 'タスク名を入力した時' do
       let(:name) { 'タスク名' }
       let(:description) { 'タスク詳細' }
+      let!(:expired_at) { Time.zone.parse('2020-12-01 00:00:00') }
 
       it '作成できる' do
         expect(page).to have_content name
         expect(page).to have_content description
-        expect(page).to have_content /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/
+        expect(page).to have_selector '.task_expired_at', text: expired_at.to_s
+        expect(page).to have_selector '.task_created_at', text: /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/
         
       end
     end
@@ -85,6 +90,7 @@ RSpec.describe 'Tasks', type: :system do
     context 'タスク名を入力しない時' do
       let(:name) { '' }
       let(:description) { 'タスク詳細' }
+      let(:expired_at) { Time.now }
 
       it 'エラーになる' do
         expect(page).to have_content 'タスク名を入力してください'
@@ -102,20 +108,21 @@ RSpec.describe 'Tasks', type: :system do
       visit edit_task_path(task)
       fill_in 'タスク名',	with: name
       fill_in 'タスク詳細',	with: description
+      fill_in '終了期限',	with: expired_at
       click_button '保存'
     end
 
     context 'タスク名編集した時' do
       let(:name) { 'タスク名(変更後)' }
       let(:description) { 'タスク詳細(変更後)' }
+      let(:expired_at) { Time.zone.parse('2020-12-01 00:00:00') }
 
       it '編集できる' do
         expect(page).to have_content name
         expect(page).to have_content description
-        expect(page).to have_content /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/
+        expect(page).to have_selector '.task_expired_at', text: expired_at.to_s
+        expect(page).to have_selector '.task_created_at', text: /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/
       end
     end
-    
   end
-
 end
