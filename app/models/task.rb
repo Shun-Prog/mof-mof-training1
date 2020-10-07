988860b0d98ceffb9ae8c78d0c887ec85e39065a
Shun-Prog <shun.prog.tanaka@gmail.com>
2020-10-07 11:40:10 +0900

    期限日に過去日付のバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,7 +14,13 @@
 class Task < ApplicationRecord
     validates :name, presence: true, length: { maximum: 30 }
     validates :description, presence: true, length: { maximum: 1000 }
+    validate :expired_at_valid?
+    
     # 一時的にコメントアウト
     # validates :priority
     # validates :status, presence: true
+
+    def expired_at_valid?        
+        errors.add(:expired_at, 'は現在日以降の日付を入力してください') if expired_at.nil? || Date.parse(expired_at.to_s) < Date.today
+    end
 end
