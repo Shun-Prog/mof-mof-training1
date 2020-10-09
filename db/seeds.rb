99fd92bd45bb30fe6a8095dc0a59c639947cdeff
Shun-Prog <shun.prog.tanaka@gmail.com>
2020-10-09 15:52:41 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
 if Rails.env.development?
+  User.delete_all
+  user = User.create!(
+    name: "ダミーユーザー",
+    email: "dummy@example.com",
+    password: "12345678"
+  )
   # タスク作成のseed
-  10000.times do |n|
+  Task.delete_all
+  100.times do |n|
     Task.create!(
       name: "タスク名#{n + 1}",
       description: "タスク詳細#{n + 1}", 
-      expired_at: Date.today
+      expired_at: Date.today,
+      user_id: user.id
     )
   end
 end
