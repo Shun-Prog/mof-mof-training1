c8b39c096f72a51978705923c50e2dc8fc8f3b4f
Shun-Prog <shun.prog.tanaka@gmail.com>
2020-10-10 22:18:51 +0900

    ログイン、ログアウト機能追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email].downcase)
+    if user&.authenticate(session_params[:password])
+      login(user)
+      flash[:success] = 'ログインしました'
+      redirect_to tasks_path
+    else
+      flash.now[:danger] = 'ログインできませんでした'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    logout if logged_in?
+    flash[:success] = 'ログアウトしました'
+    redirect_to login_path
+  end
+
+
+  private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,25 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  has_secure_password
+  has_many :tasks, dependent: :destroy
+
+  validates :name,
+    presence: true,
+    length: { maximum: 30 }
   
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+    presence: true,
+    format: VALID_EMAIL_REGEX,
+    uniqueness: { case_sensitive: false }
+
+  validates :password,
+    presence: true,
+    length: { minimum: 8 }
+
+  before_save do
+    self.email.downcase! # メールアドレスは小文字で統一 
+  end
+
 end
