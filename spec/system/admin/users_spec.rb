e6608fe769558d20ba597775c08bbf5257dc65dd
Shun-Prog <shun.prog.tanaka@gmail.com>
2020-10-13 18:03:36 +0900

    管理者ユーザー追加に伴うテスト追加

--- spec/system/admin/users_spec.rb
+++ spec/system/admin/users_spec.rb
@@ -7,16 +7,37 @@ RSpec.describe "Users", type: :system do
 
   describe "一覧" do
 
-    before do
-      login(user)
-      visit admin_users_path
+    context "管理者ユーザーの場合" do
+
+      before do
+        login(user)
+        visit admin_users_path
+      end
+
+      it "ユーザー一覧が表示される" do
+        expect(page).to have_content user.name
+        expect(page).to have_content user.email
+        expect(page).to have_content user.tasks.count
+        expect(page).to have_content user.admin? ? "有り" : "無し"
+      end
+
     end
 
-    it "ユーザー一覧が表示される" do
-      expect(page).to have_content user.name
-      expect(page).to have_content user.email
-      expect(page).to have_content user.tasks.count
+    context "管理者権限の無いユーザーの場合" do
+
+      let(:non_admin_user) { create(:user, admin: false) }
+
+      before do
+        login(non_admin_user)
+        visit admin_users_path
+      end
+
+      it "ルートページが表示される" do
+        expect(page).to have_current_path root_path
+      end
+
     end
+    
 
   end
 
@@ -30,6 +51,7 @@ RSpec.describe "Users", type: :system do
     it "ユーザー詳細が表示される" do
       expect(page).to have_content user.name
       expect(page).to have_content user.email
+      expect(page).to have_content user.admin? ? "有り" : "無し"
     end
     
   end
@@ -41,7 +63,9 @@ RSpec.describe "Users", type: :system do
       visit new_admin_user_path
       fill_in "user_name",	with: name
       fill_in "user_email",	with: email
-      fill_in "user_password",	with: "12345678"
+      fill_in "user_password",	with: password
+      fill_in "user_password_confirmation",	with: password
+      uncheck "user_admin"
       click_button "保存"
     end
 
@@ -49,10 +73,12 @@ RSpec.describe "Users", type: :system do
 
       let(:name) { "ユーザー名" }
       let(:email) { "test@example.com" }
+      let(:password) { "12345678" }
 
       it "ユーザーを作成できる" do
         expect(page).to have_content name
         expect(page).to have_content email
+        expect(page).to have_content "無し"
       end
 
     end
@@ -66,36 +92,115 @@ RSpec.describe "Users", type: :system do
       visit edit_admin_user_path(user)
       fill_in "user_name",	with: name
       fill_in "user_email",	with: email
-      fill_in "user_password",	with: "12345678"
-      click_button "保存"
+      fill_in "user_password",	with: password
+      fill_in "user_password_confirmation",	with: password
     end
 
     context "編集した時" do
 
       let(:name) { "ユーザー名(変更語)" }
       let(:email) { "test_modified@example.com" }
+      let(:password) { "12345678" }
 
       it "ユーザーを編集できる" do
+        click_button "保存"
         expect(page).to have_content name
         expect(page).to have_content email
+        expect(page).to have_content "有り"
       end
 
     end
+
+    context "管理者ユーザー2人以上の状態で管理者権限を外した時" do
+
+      let!(:admin_user) { create(:user) }
+
+      let(:name) { "ユーザー名(変更語)" }
+      let(:email) { "test_modified@example.com" }
+      let(:password) { "12345678" }
+
+      before do
+        uncheck "user_admin"
+        click_button "保存"
+      end
+
+      it "ルートページが表示される" do
+        expect(page).to have_current_path root_path
+      end
+
+    end
+
+    context "管理者ユーザー1人の状態で管理者権限を外した時" do
+
+      let(:name) { "ユーザー名(変更語)" }
+      let(:email) { "test_modified@example.com" }
+      let(:password) { "12345678" }
+
+      before do
+        uncheck "user_admin"
+        click_button "保存"
+      end
+
+      it "エラーになる" do
+        expect(page).to have_content "は最低1ユーザー保持する必要があります"
+      end
       
+    end
+    
   end
 
   describe "削除" do
 
-    before do
-      login(user)
-      visit admin_user_path(user)
-      page.accept_confirm do
-        click_on "削除"
+    context "管理者ユーザー以外を削除した場合" do
+
+      let!(:non_admin_user) { create(:user, admin: false) }
+
+      before do
+        login(user)
+        visit admin_user_path(non_admin_user)
+        page.accept_confirm do
+          click_on "削除"
+        end
       end
+  
+      it "ユーザーを削除できる" do
+        expect(page).to have_content "ユーザーを削除しました"
+      end
+      
     end
 
-    it "ユーザーを削除できる" do
-      expect(page).to have_content "ユーザーを削除しました"
+    context "管理者ユーザー2人以上の状態で管理者を削除した場合" do
+
+      let!(:admin_user) { create(:user) }
+
+      before do
+        login(user)
+        visit admin_user_path(admin_user)
+        page.accept_confirm do
+          click_on "削除"
+        end
+      end
+  
+      it "ユーザーを削除できる" do
+        expect(page).to have_content "ユーザーを削除しました"
+      end
+      
+    end
+
+    context "管理者ユーザー1人の状態で管理者を削除した場合" do
+
+      before do
+        login(user)
+        visit admin_user_path(user)
+        page.accept_confirm do
+          click_on "削除"
+        end
+      end
+  
+      it "エラーになる" do
+        expect(page).to have_content "は最低1ユーザー保持する必要があります"
+      end
+
     end
 
   end
