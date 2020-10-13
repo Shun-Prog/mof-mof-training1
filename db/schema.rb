dd7233cba6f555c6fca1f2033f2a053a7de892d2
Shun-Prog <shun.prog.tanaka@gmail.com>
2020-10-13 15:59:00 +0900

    Userモデルにadmin追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_052930) do
+ActiveRecord::Schema.define(version: 2020_10_13_014038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_10_09_052930) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
   end
 
   add_foreign_key "tasks", "users"
